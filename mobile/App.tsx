c68c150355cacca2561abdb4729a3150c3943c60
Carlos Alves <carlosalves369m@hotmail.com>
2022-11-04 10:34:33 -0300

    :heavy_plus_sign: add native-base dependencies

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NativeBaseProvider } from 'native-base'; 
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello world!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#202',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
